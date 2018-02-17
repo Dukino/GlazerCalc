9c3a9ce02c107b1a0a0fab158c03cd189a3cabaa
Dukino <bro14078@byui.edu>
2018-02-17 13:46:49 -0700

    Final Version

---
+++ GlazerCalc/Glazer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace GlazerCalc
+{
+    class Glazer
+    {
+        private double width      { get; set; }
+        private double height     { get; set; }
+        private double woodLength { get; set; }
+        private double glassArea  { get; set; }
+        private String tintColor  { get; set; }
+        private int    quantity   { get; set; }
+
+        public Glazer(double width, double height, double woodLength, double glassArea,
+            String tintColor, int quantity)
+        {
+            this.width      = width;
+            this.height     = height;
+            this.woodLength = woodLength;
+            this.glassArea  = glassArea;
+            this.tintColor  = tintColor;
+            this.quantity   = quantity;
+        }     
+        
+        public void DisplayResults(TextBlock header, TextBlock textBlock)
+        {
+            header.Text = "Review your Calculations";
+            textBlock.Text = "Width: " + width +
+                "\nHeight: " + height +
+                "\nWood Length: " + woodLength +
+                "\nGlass Area: " + glassArea +
+                "\nTint Color: " + tintColor +
+                "\nQuantity: " + quantity; 
+        }
+    }
+}
--- GlazerCalc/MainPage.xaml.cs
+++ GlazerCalc/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -17,14 +18,132 @@ using Windows.UI.Xaml.Navigation;
 
 namespace GlazerCalc
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
+        double width, height, woodLength, glassArea;
+
+        private bool ValidateDimensions(object sender, RoutedEventArgs e)
+        {
+            bool isEmpty = string.IsNullOrWhiteSpace(WidthInput.Text) && string.IsNullOrWhiteSpace(HeightInput.Text);
+            if(isEmpty)
+            {
+                DisplayInvalidDimensionsDialog();
+                return false;
+            }
+
+            width = Convert.ToDouble(WidthInput.Text);
+            height = Convert.ToDouble(HeightInput.Text);
+            if ((width <= 5.0 && width >= 0.5) && (height <= 3.0 && height >= 0.75))
+            {
+                return true;
+            }
+            else
+            {
+                DisplayInvalidDimensionsDialog();
+                return false;
+            }
+        }
+
+        private bool ValidateTintSelection(object sender, RoutedEventArgs e)
+        {
+            if (Convert.ToBoolean(BlackTint.IsChecked) || Convert.ToBoolean(BlueTint.IsChecked) ||
+                Convert.ToBoolean(BrownTint.IsChecked))
+            {
+                return true;
+            }
+            else
+            {
+                DisplayNoSelectionDialog();
+                return false;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (ValidateDimensions(sender, e) && ValidateTintSelection(sender, e))
+            {
+                width = Convert.ToDouble(WidthInput.Text);
+                height = Convert.ToDouble(HeightInput.Text);
+                woodLength = 2 * (width + height) * 3.25;
+                glassArea = 2 * (width * height);
+                String tintColor;
+                if (Convert.ToBoolean(BlackTint.IsChecked))
+                {
+                    tintColor = Convert.ToString(BlackTint.Content);
+                }
+                else if (Convert.ToBoolean(BlueTint.IsChecked))
+                {
+                    tintColor = Convert.ToString(BlueTint.Content);
+                }
+                else
+                {
+                    tintColor = Convert.ToString(BrownTint.Content);
+                }
+                int quantity = Convert.ToInt32(quantitySlider.Value);
+
+                Glazer glazer = new Glazer(width, height, woodLength, glassArea, tintColor, quantity);
+                glazer.DisplayResults(ResultsHeader, Results);
+            }
+        }
+
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
         }
+
+        void widthKeyUp(object sender, RoutedEventArgs e)
+        {
+            bool isDouble = Double.TryParse(WidthInput.Text, out double widthValue);
+            if (!isDouble && !string.IsNullOrWhiteSpace(WidthInput.Text))
+            {
+                DisplayInvalidValueDialog();
+            }
+        }
+
+        void heightKeyUp(object sender, RoutedEventArgs e)
+        {
+            bool isDouble = Double.TryParse(HeightInput.Text, out double heightValue);
+            if (!isDouble && !string.IsNullOrWhiteSpace(HeightInput.Text))
+            {
+                DisplayInvalidValueDialog();
+            }
+        }
+
+        private async void DisplayInvalidValueDialog()
+        {
+            ContentDialog invalid = new ContentDialog
+            {
+                Title = "Invalid character",
+                Content = "Please enter a valid measurement for the width and height. All values must but numerical.",
+                CloseButtonText = "Ok"
+            };
+
+            ContentDialogResult result = await invalid.ShowAsync();
+        }
+
+        private async void DisplayNoSelectionDialog()
+        {
+            ContentDialog noSelection = new ContentDialog
+            {
+                Title = "No Selection",
+                Content = "Please select the tint color for your order.",
+                CloseButtonText = "Ok"
+            };
+
+            ContentDialogResult result = await noSelection.ShowAsync();
+        }
+
+        private async void DisplayInvalidDimensionsDialog()
+        {
+            ContentDialog invalidDimensions = new ContentDialog
+            {
+                Title = "Invalid Dimensions",
+                Content = "Invalid Width or Height entered. Please enter a width between 0.5ft to 5.0ft and a height between 0.75ft to 3.0ft.",
+                CloseButtonText = "Ok"
+            };
+
+            ContentDialogResult result = await invalidDimensions.ShowAsync();
+        }
+
     }
 }
